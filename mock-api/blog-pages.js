096aaa51c426d7210134acb26e58a9ecae9409ea
David Broadlick <david@aceable.com>
2020-07-07 17:17:14 -0500

    First commit.

---
+++ mock-api/blog-pages.js
@@ -0,0 +1,19 @@
+export default {
+    posts: [
+        {
+            route: 'how-to-use-nuxt',
+            title: 'How To Use Nuxt'
+        },
+        {
+            route: 'nuxt-performance',
+            title: 'Getting The Most Out Of Nuxt'
+        },
+        {
+            route: 'nuxt-vs-next',
+            title: 'A Comparison of Nuxt vs Next'
+        },
+        {
+            route: 'nuxt-ssr-guide',
+        }
+    ]
+}
